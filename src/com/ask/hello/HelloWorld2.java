0092111e68a0c89de2b441fd8176cc22a650c326
Sarath Ambadas <sarath.ambadas@gmail.com>
2016-09-15 12:43:59 +0530

    modified

--- src/com/ask/hello/HelloWorld2.java
+++ src/com/ask/hello/HelloWorld2.java
@@ -3,7 +3,7 @@ package com.ask.hello;
 public class HelloWorld2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World2 !!");
+		System.out.println("Hello World2 !!!!");
 
 	}
 
